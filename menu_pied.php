d5a86f16c02ddc3d05b0285977de10dafb2936b6
gil.deluermoz <gil.deluermoz@ecrins-parcnational.fr>
2015-10-12 15:39:14 +0200

    close #13 -

--- menu_pied.php
+++ menu_pied.php
@@ -1,7 +1,10 @@
 <div id="bas">
 	<?
 		$an = date("Y");
+        $version_file = fopen('VERSION', 'r');
+        $num_version = fgets($version_file);
+        fclose($version_file);
 	?>
-	2009 - <? echo $an ?> / <? echo $etablissement ?> / D&eacute;veloppement : <a href="http://www.ecrins-parcnational.fr">Parc national des Ecrins</a> / Version 2.1
+	2009 - <? echo $an ?> / <? echo $etablissement ?> / D&eacute;veloppement : <a href="http://www.ecrins-parcnational.fr">Parc national des Ecrins</a> / Version <? echo $num_version;?>
 </div>
 
